e6810232c24269b9d1c9dba196c376fdc242505d
Carlos ' Arjona <eduardo.arjona@hotmail.com>
2017-09-24 23:49:00 -0300

    criar controller

---
+++ teste55/Controllers/EstudanteController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using teste55.Models;
+
+namespace teste55.Controllers
+{
+    public class EstudanteController : Controller
+    {
+        private EstudanteContext db = new EstudanteContext();
+
+        // GET: Estudante
+        public ActionResult Index()
+        {
+            return View(db.Estudantes.ToList());
+        }
+
+        // GET: Estudante/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudante estudante = db.Estudantes.Find(id);
+            if (estudante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudante);
+        }
+
+        // GET: Estudante/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Estudante/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EstudanteId,Nome,Email,CPF")] Estudante estudante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Estudantes.Add(estudante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(estudante);
+        }
+
+        // GET: Estudante/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudante estudante = db.Estudantes.Find(id);
+            if (estudante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudante);
+        }
+
+        // POST: Estudante/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EstudanteId,Nome,Email,CPF")] Estudante estudante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(estudante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(estudante);
+        }
+
+        // GET: Estudante/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Estudante estudante = db.Estudantes.Find(id);
+            if (estudante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(estudante);
+        }
+
+        // POST: Estudante/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Estudante estudante = db.Estudantes.Find(id);
+            db.Estudantes.Remove(estudante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
